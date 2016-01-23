19ccb8c4050940148d4f830702ca50c846a9f3ab
Reiuiji <reiuiji@gmail.com>
2016-01-23 04:28:59 -0500

    Updated lua script for stable version

--- VLC-Song-Track.lua
+++ VLC-Song-Track.lua
@@ -1,31 +1,42 @@
 --[[
-VLC Song Track Extension for VLC media player
-Copyright 2015 Reiuiji
+VLC Song Tracker Extension for VLC media player
+Copyright 2016 Reiuiji
 
 Authors: Reiuiji
 Contact: reiuiji@gmail.com
 
 Information:
-Keeps track of what songs were played in VLC and saves the meta information and time played to a csv file which can be parsed by any spreadsheet application (LibreOffice Calc).
+Keeps track of which songs were played in VLC and saves the song information as well as the time played to a csv file which can be parsed by any spreadsheet application (LibreOffice Calc).
 
 -------------------------
 Installation Instructions
 -------------------------
 
-Place this file in the corresponding folder and restart vlc.
+Place this file in the corresponding folder and restart vlc or reload plugin extensions.
 
-Linux: ~/.local/share/vlc/lua/extensions/
+Linux:
+  Current User: ~/.local/share/vlc/lua/extensions/
+     All Users: /usr/lib/vlc/lua/extensions/
 
-Windows: %APPDATA%\vlc\lua\extensions
+Windows:
+  Current User: %APPDATA%\vlc\lua\extensions
+     All Users: %ProgramFiles%\VideoLAN\VLC\lua\extensions\
 
-Mac OS X: lua/{playlist,meta,intf}/
+Mac OS X:
+  Current User: /Users/%your_name%/Library/Application Support/org.videolan.vlc/lua/extensions/
+     All Users: /Applications/VLC.app/Contents/MacOS/share/lua/extensions/
+
+The SongList.csv will be saved in the vlc user director which can be found in the following places
+    Linux: ~/.local/share/vlc/SongList.csv
+  Windows: %APPDATA%\vlc\SongList.csv
+ Mac OS X: /Users/%your_name%/Library/Application Support/org.videolan.vlc/SongList.csv
 
 --]]
 
 -- Global Variables
 -- Name of the file to save the songs
-FileName = "VLCSongTrack.csv"
-SongTrackFile = ""
+FileName = "SongList.csv"
+SongTrackerFile = ""
 -- Default header for the csv file
 FileHeader = "Date,Time,Title,Artist,Album,Genre,Comments,Location\n"
 -- CSV variable normally ","
@@ -34,63 +45,63 @@ CSVValue = "|"
 -- Descriptor
 function descriptor()
   return {
-    title = "VLC Song Track 0.0.6",
-    version = "0.0.6",
+    title = "VLC Song Tracker 0.1.0",
+    version = "0.1.0",
     author = "Reiuiji",
     url = "https://github.com/Reiuiji/VLCAddons",
-    shortdesc = "VLC Song Track",
-    description = "Keeps track of what songs were played in VLC and saves the meta information and time played to <UserDataDirectory>\VLCSongTrack.csv which can be parsed by any spreadsheet application (LibreOffice Calc).",
+    shortdesc = "VLC Song Tracker",
+    description = "Keeps track of what songs were played in VLC and saves the meta information and time played to SongList.csv which can be parsed by any spreadsheet application (LibreOffice Calc).",
     capabilities = { "input-listener" }
   }
 end
 
 -- Activate plugin
 function activate()
-  vlc.msg.dbg("[VLC Song Track] -- Activate")
+  vlc.msg.dbg("[VLC Song Tracker] -- Activate")
   init()
-  update_song_track()
+  update_song_Tracker()
 end
 
 -- Deactivate Plugin
 function deactivate()
-  vlc.msg.dbg("[VLC Song Track] -- Deactivate")
+  vlc.msg.dbg("[VLC Song Tracker] -- Deactivate")
 end
 
 -- Close Trigger
 function close()
-  vlc.msg.dbg("[VLC Song Track] -- Close")
+  vlc.msg.dbg("[VLC Song Tracker] -- Close")
 end
 
 -- Triggers
 function input_changed()
-  vlc.msg.dbg("[VLC Song Track] -- Input Changed")
-  update_song_track()
+  vlc.msg.dbg("[VLC Song Tracker] -- Input Changed")
+  update_song_Tracker()
 end
 function meta_changed()
-  vlc.msg.dbg("[VLC Song Track] -- Meta Changed")
-  update_song_track()
+  vlc.msg.dbg("[VLC Song Tracker] -- Meta Changed")
+  update_song_Tracker()
 end
 
 -- First time init file
 function init()
-  vlc.msg.dbg("[VLC Song Track] Initializing plugin")
-  SongTrackFile = vlc.config.userdatadir() .. "/" .. FileName
-  vlc.msg.dbg("[VLC Song Track] Song Track File: " .. SongTrackFile .. ")")
+  vlc.msg.dbg("[VLC Song Tracker] Initializing plugin")
+  SongTrackerFile = vlc.config.userdatadir() .. "/" .. FileName
+  vlc.msg.dbg("[VLC Song Tracker] Song Tracker File: " .. SongTrackerFile .. ")")
   -- Check if the file exist
-  local file = io.open(SongTrackFile,"r")
+  local file = io.open(SongTrackerFile,"r")
   if file ~= nil then
     io.close(file)
   else
-    vlc.msg.dbg("[VLC Song Track] File does not exist, Creating Header")
-    local file = io.open(SongTrackFile, "w+")
+    vlc.msg.dbg("[VLC Song Tracker] File does not exist, Creating Header")
+    local file = io.open(SongTrackerFile, "w+")
     file:write(FileHeader)
     file:close()
   end
 end
 
--- Update Track
-function update_song_track()
-  vlc.msg.dbg("[VLC Song Track] Update Song Track!")
+-- Update Tracker
+function update_song_Tracker()
+  vlc.msg.dbg("[VLC Song Tracker] Update Song Tracker!")
   if vlc.input.is_playing() then
     local item = vlc.item or vlc.input.item()
     if item then
@@ -144,8 +155,8 @@ end
 
 -- Write File
 function write_file(info)
-  vlc.msg.dbg("[VLC Song Track] Write File!")
-  local file = io.open(SongTrackFile, "a")
+  vlc.msg.dbg("[VLC Song Tracker] Write File!")
+  local file = io.open(SongTrackerFile, "a")
   file:write(info .. "\n")
   file:close()
 end
